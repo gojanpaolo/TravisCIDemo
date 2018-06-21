3154bdf47a27ad43d8dadd68b1f1481f7e24c065
gojanpaolo <go.janpaolo@gmail.com>
2018-06-21 16:53:03 -0500

    Added MyNetStandard

---
+++ MySolution/MyNetStandard.Tests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace MyNetStandard.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest() => Assert.True(true);
+
+        [Fact]
+        public void FailingTest() => Assert.True(false);
+    }
+}
---
+++ MySolution/MyNetStandard/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MyNetStandard
+{
+    public class Class1
+    {
+    }
+}
